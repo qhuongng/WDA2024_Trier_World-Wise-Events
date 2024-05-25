2166e9cc9eae115f2d728c02283e73e7b77bb682
qhuongng <nq.huong.2304@gmail.com>
2024-05-25 18:48:21 +0700

    Update CORS options

--- server/src/App.js
+++ server/src/App.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOption = {
-  origin: [process.env.SELLER_URL, process.env.USER_URL],
+  origin: process.env.USER_URL,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
